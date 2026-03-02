4166e538514ad091976c871283ebe31f703f4faf
P1_vr <p1vr1013@gmail.com>
2026-03-02 21:30:04 +0000

    l

--- WebsiteCode/script.js
+++ WebsiteCode/script.js
@@ -51,7 +51,7 @@ container.appendChild(loadingDiv);
 const loader = new THREE.FBXLoader();
 
 loader.load(
-    'Models/A with arrows V3.fbx', // <-- CHANGE THIS TO YOUR MODEL NAME
+    'Models/Arrow V1.fbx', // <-- CHANGE THIS TO YOUR MODEL NAME
     function (object) {
         model = object;
         
