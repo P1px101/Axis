5fc8a047a9ccfcc234749cdfc87b5fb7c4ac5f33
P1_vr <p1vr1013@gmail.com>
2026-03-02 20:35:20 +0000

    Update script.js

--- WebsiteCode/script.js
+++ WebsiteCode/script.js
@@ -51,7 +51,7 @@ container.appendChild(loadingDiv);
 const loader = new THREE.FBXLoader();
 
 loader.load(
-    'Models/A with arrows V3.fbx', // <-- CHANGE THIS TO YOUR MODEL NAME
+    'Models/A with arrows V2.fbx', // <-- CHANGE THIS TO YOUR MODEL NAME
     function (object) {
         model = object;
         
