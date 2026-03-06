bdf0429f2d10b511760a5f8e0a81b7a3d1e1b197
Golonka <milan.mich08@gmail.com>
2026-03-06 02:34:57 +0100

    Update profilecard.js

--- WebsiteCode/JS/profilecard.js
+++ WebsiteCode/JS/profilecard.js
@@ -1,4 +1,4 @@
-//its me Golonka himself with some fixes again lmao I hope it works now I rewrited it like 3 times
+//its me Golonka himself with some fixes again lmao I hope it works now I rewrited it like 3 times; Update : 4 times
 const cards = document.querySelectorAll(".profile-card");
 
 cards.forEach(card => {
@@ -6,12 +6,12 @@ cards.forEach(card => {
     const content = card.querySelector(".profile-card-expanded");
 
     btn.addEventListener("click", () => {
-        const isExpanded = card.classList.contains("expanded");
-
-        if (isExpanded) {
+        // Jeśli już jest rozwinięta -> zamknij
+        if (card.classList.contains("expanded")) {
             content.style.maxHeight = null;
             card.classList.remove("expanded");
         } else {
+            // Otwórz tylko tę kartę
             content.style.maxHeight = content.scrollHeight + "px";
             card.classList.add("expanded");
         }
