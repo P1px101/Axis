2718c8a39b06887a1a5647cd6e3281524eebb2b9
Golonka <milan.mich08@gmail.com>
2026-03-06 02:47:40 +0100

    Update profilecard.js

--- WebsiteCode/JS/profilecard.js
+++ WebsiteCode/JS/profilecard.js
@@ -1,8 +1,8 @@
-//its me Golonka himself with some fixes again lmao I hope it works now I rewrited it like 3 times; Update : 4 times
+//its me Golonka himself with some fixes again lmao I hope it works now I rewrited it like 3 times; Update : 6 times
 const cards = document.querySelectorAll(".profile-card");
 
 cards.forEach(card => {
-    const btn = card.querySelector(".profile-expand-btn");
+    const btn = card.querySelector(".profile-expand-btn"); 
     const content = card.querySelector(".profile-card-expanded");
 
     btn.addEventListener("click", () => {
